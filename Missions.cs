a286072bd0e6aec93f11ec180158ac18a515737f
Xenne <info@xenne.eu>
2025-05-08 11:25:48 +0200

    Initial project
    Initial Windows Forms Visual Studio project

---
+++ Missions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpansionTrader_Editor
+{
+    public class Missions
+    {
+        public static string SelectedMission { get; private set; }
+
+        public static void SelectMission()
+        {
+            List<string> tempMissions = new List<string>();
+
+            foreach (string dir in Directory.GetDirectories(ServerDirectory.MpMissionsFolderPath))
+            {
+                tempMissions.Add(dir);
+
+            }
+
+
+            MissionSelector selector = new MissionSelector(tempMissions);
+            selector.Show();
+
+        }
+
+       
+    }
+}
---
+++ ServerDirectory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExpansionTrader_Editor
+{
+    public class ServerDirectory
+    {
+
+        public static string ServerRootDirectoryPath { get; private set; }
+        public static string ProfilesFolderPath { get; private set; }
+        public static string MpMissionsFolderPath { get; private set; }
+        public static string SelectedMissionFolderPath { get; private set; }
+
+        public static void SetServerDirectory()
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.ShowDialog();
+
+            ServerRootDirectoryPath = folderBrowserDialog.SelectedPath;
+            ProfilesFolderPath = ServerRootDirectoryPath + "\\profiles\\";
+            MpMissionsFolderPath = ServerRootDirectoryPath + "\\mpmissions\\";
+            MessageBox.Show(ProfilesFolderPath);
+
+        }
+
+        public static void ValidateDirectories()
+        {
+
+            if (!Directory.Exists(ServerRootDirectoryPath))
+            {
+                MessageBox.Show("Server folder does not exist!");
+                return;
+            }
+
+            if (!Directory.Exists(ProfilesFolderPath))
+            {
+                MessageBox.Show("Profiles folder not found!");
+                return;
+            }
+
+            if (!Directory.Exists(MpMissionsFolderPath))
+            {
+                MessageBox.Show("MpmMissions folder not found!");
+                return;
+            }
+
+
+        }
+
+
+    }
+}
